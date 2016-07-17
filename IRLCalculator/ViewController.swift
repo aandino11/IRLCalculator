aa354638213f692ba3a280ca35e3d371917c3bbd
aandino11 <aandino11@gmail.com>
2016-07-17 14:53:48 -0400

    Initial Commit

---
+++ IRLCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IRLCalculator
+//
+//  Created by Alvin  Andino  on 7/17/16.
+//  Copyright © 2016 Alvin  Andino . All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
